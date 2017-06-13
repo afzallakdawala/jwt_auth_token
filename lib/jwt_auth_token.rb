778fb0da5a5cb60cc5632f9c008931d16e9938a0
Afzal Lakdawala <afzal@embibe.com>
2017-06-13 14:36:09 +0530

    gem file changes for user auth

--- lib/jwt_auth_token.rb
+++ lib/jwt_auth_token.rb
@@ -1,9 +1,45 @@
-class JwtAuthToken 
+module JwtAuthToken
+end
+require 'jwt'
+def jwt_hmac_secret
+  @_jwt_hmac_secret ||= Rails.application.secrets[:secret_key_base]
+end
 
-  def self.set_header(response, data)
-    hmac_secret = Rails.application.secrets[:secret_key_base]
-    payload_data = JWT.encode(data,hmac_secret,'HS512')
-    response.set_header("embibe-token", payload_data)
+def jwt_algorithm
+  @_jwt_algorithm ||= 'HS512'
+end
+
+def header_name
+  @_header_name ||= "embibe-token"
+end
+
+def jwt_set_header(data)
+  encoded_token = JWT.encode(data,jwt_hmac_secret,jwt_algorithm)
+  response.set_header(header_name, encoded_token)
+end
+
+def authenticate_user?
+  if is_valid_token?
+    return true
+  else
+    render json: {"error" => "User Authentication Failed", :status => 401}, :status => 401 and return
   end
+end
 
+def current_user
+  @_current_user ||= OpenStruct.new(@decoded_token) if is_valid_token?
+end
+
+def is_valid_token?
+  begin
+    token = request.headers[header_name]
+    @decoded_token = JSON.parse(JWT.decode(token, jwt_hmac_secret, true, { :algorithm => jwt_algorithm })[0])
+    return validate_keys
+  rescue Exception => e
+    return false
+  end  
+end
+
+def validate_keys
+  !!@_validate_keys ||= (@decoded_token.keys && ["id", "email"]).any?
 end
